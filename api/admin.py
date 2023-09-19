5ba8c827a75ba4c48d2f56537babfd7388fcdf3e
ishubhamsingh2e <ishubhamsingh2e@gmail.com>
2023-09-19 12:17:29 +0530

    added: iconpacks model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Wallpaper, Tag
+from .models import Wallpaper, Tag, IconPack
+
 # Register your models here.
 
 admin.site.register(Wallpaper)
+admin.site.register(IconPack)
 admin.site.register(Tag)
--- api/models.py
+++ api/models.py
@@ -9,6 +9,22 @@ def generate_filename(instance, filename):
     return f"wallpapers/{filename}"
 
 
+def generate_iconpackname(instance, filename):
+    # Generate a unique filename using a UUID
+    extension = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{extension}"
+    return f"icon-packs/{filename}"
+
+
+class IconPack(models.Model):
+    name = models.CharField(max_length=100)
+    icon_pack = models.FileField(upload_to=generate_iconpackname)
+    tags = models.ManyToManyField('Tag', related_name='icon_packs')
+
+    def __str__(self):
+        return self.name
+
+
 class Wallpaper(models.Model):
     title = models.CharField(max_length=100)
     image_url = models.ImageField(upload_to=generate_filename)
