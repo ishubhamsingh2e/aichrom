07c35dbb32c01220bf239b2b0ac30d2361a27c6b
ishubhamsingh2e <ishubhamsingh2e@gmail.com>
2023-09-22 21:07:55 +0530

    update: no-cache

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Build the project
@@ -8,7 +7,3 @@ python3.9 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py compress --force
