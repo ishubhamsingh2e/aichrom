7d2e555d4e7d7aa524be26a83f9ffd30ee558507
ishubhamsingh2e <ishubhamsingh2e@gmail.com>
2024-05-15 14:07:46 +0530

    fix

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GetTransaction, WallpaperListView, IconPackApiView, SendOTP, VerifyOTP, GetPreferenceImage, GetPreferenceSchema, TransactionSchema
+from .views import GetTransaction, WallpaperListView, IconPackApiView, SendOTP, VerifyOTP, GetPreferenceImage, GetPreferenceSchema, TransactionSchema, WallpaperTransactionSchema
 
 urlpatterns = [
     path('wallpapers/', WallpaperListView.as_view(), name='wallpapers'),
@@ -9,6 +9,6 @@ urlpatterns = [
     path("send_otp/", SendOTP.as_view(), name="send-otp"),
     path("verify_otp/", VerifyOTP.as_view(), name="verify-otp"),
     path("transaction/", TransactionSchema.as_view(), name="transaction"),
-    path("wallpaper_transaction/", TransactionSchema.as_view(), name="transaction"),
+    path("wallpaper_transaction/", WallpaperTransactionSchema.as_view(), name="transaction"),
     path("restore/", GetTransaction.as_view(), name="transaction")
 ]
